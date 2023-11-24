f47f7d85e54b04065c3e741edc7d6a60f96217ce
Park Jae myung <kbtapjm@gmail.com>
2023-11-25 00:29:12 +0900

    #1 updated get diver api

---
+++ src/main/java/io/divetrip/dto/ResourcesDto.java
@@ -0,0 +1,15 @@
+package io.divetrip.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ResourcesDto {
+
+}
