e52b498a6016abc1ffa4ab608e9a527d5159089a
jmpark <kbtapjm@gmail.com>
2024-12-10 16:08:00 +0900

    #9 get trip reservation payment api 추가

--- src/main/java/io/divetrip/domain/repository/PaymentRepository.java
+++ src/main/java/io/divetrip/domain/repository/PaymentRepository.java
@@ -1,12 +1,16 @@
 package io.divetrip.domain.repository;
 
 import io.divetrip.domain.entity.Payment;
+import io.divetrip.domain.entity.TripReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+
+    List<Payment> findByTripReservation(final TripReservation tripReservation);
 
 }
