b20c98cf8d958ee4ab69047cc7d1d37baac63804
jmpark <kbtapjm@gmail.com>
2025-03-19 17:39:45 +0900

    #10 resource api 수정

--- src/main/java/io/divetrip/library/domain/repository/ResourceRepository.java
+++ src/main/java/io/divetrip/library/domain/repository/ResourceRepository.java
@@ -4,11 +4,14 @@ import io.divetrip.library.domain.entity.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ResourceRepository extends JpaRepository<Resource, UUID> {
 
     boolean existsByResourceUrl(final String resourceUrl);
+
+    List<Resource> findByOrderByResourceOrderAsc();
 
 }
